a5e9ca85909fcd456cd7932174bada833f7a7422
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-15 22:24:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,9 +4,9 @@ def prime?(number)
   
   range = (2..number-1).to_a
   
-  if number < 1 
+  if number <=  1 
     false 
-  elsif number == 1 || number == 2
+  elsif number == 2
     TRUE
   elsif range.any?{|factor| number % factor == 0}
     false
