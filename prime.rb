9fa64b44af67771fcc2370711c2ca16dc49150ed
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-15 22:28:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,27 @@
 require 'pry'
 
+# def prime?(number)
+  
+#   range = (2..number-1).to_a
+  
+#   if number <=  1 
+#     false 
+#   elsif number == 2
+#     TRUE
+#   elsif range.any?{|factor| number % factor == 0}
+#     false
+#   else
+#     TRUE
+#   end
+  
+# end
+
 def prime?(number)
   
-  range = (2..number-1).to_a
+  limit = Integer.sqrt(number)
+  int_limit = limit.to_i
+  
+  range = (2..int_limit).to_a
   
   if number <=  1 
     false 
@@ -15,4 +34,3 @@ def prime?(number)
   end
   
 end
-
