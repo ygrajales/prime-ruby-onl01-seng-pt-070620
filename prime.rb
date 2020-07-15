ca650e13207df68ca0b5dc16f087cda31cfedc08
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-15 22:29:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+  
+  range = (2..number-1).to_a
+  
+  if number <=  1 
+    false 
+  elsif number == 2
+    TRUE
+  elsif range.any?{|factor| number % factor == 0}
+    false
+  else
+    TRUE
+  end
+  
+end
