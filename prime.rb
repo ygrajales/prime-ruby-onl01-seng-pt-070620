eb9f858c967c6ed7695763f28557cccc0e2d2b47
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-15 22:05:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+  
+  limit = Integer.sqrt(number)
+  int_limit = limit.to_i
+  range = [2..int_limit]
+  
+  binding.pry
+  
+  if number < 2
+    false
+  elsif number == 2 || number == 3
+    TRUE
+  elsif range.any?{|factor| number % factor == 0}
+    FALSE
+  else
+    TRUE
+  end
+  
+end
